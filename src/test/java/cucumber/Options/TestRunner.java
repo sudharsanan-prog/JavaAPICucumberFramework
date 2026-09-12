a19103dbcb2eeda7129eb5a229072c6f3586b86e
Sudharsanan Rajavelu <sudharsans762@gmail.com>
2026-09-12 09:49:09 +0530

    updated cucumber plugin in pom

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -1,7 +1,5 @@
 package cucumber.Options;
 
-// Import JUnit's AfterClass instead of TestNG
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
@@ -10,15 +8,12 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
-        plugin = {"pretty", "json:target/jsonReports/cucumber-report.json"},
+        plugin = {
+                "pretty",
+                "json:target/jsonReports/cucumber-report.json"
+        },
         glue = {"stepDefinition"},
         tags = "@DeletePlace"
 )
 public class TestRunner {
-
-    @AfterClass
-    public static void writeExtentReport() {
-        // Call the method to generate the HTML report
-        HtmlReportGenerator.generateReport();
-    }
 }
